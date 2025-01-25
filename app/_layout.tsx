ccd3db8f510fb834e05fdfd98a86f60bd81ce78a
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2025-01-25 14:51:46 +0530

    Replace nobottombar layout with a new layout structure using Stack for improved navigation and header management

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return <Stack
+    screenOptions={{
+      headerShown: false,
+    }} >;
+    <Stack.Screen name="(nobottombar)/accountinfo"
+      options={{ headerShown: true, headerTitle: "Account Info", headerBackTitle: "Go Back" }} />
+  </Stack>
+}
